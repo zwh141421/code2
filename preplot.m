1fa30851d60f9e265c6a1be7f660e110f760f1ac
zwh <zhangweihang2233@outlook.com>
2024-08-20 09:43:18 +0800

    add preplot.m

---
+++ preplot.m
@@ -0,0 +1,38 @@
+set(groot,'DefaultTextInterpreter', 'LaTeX');
+set(groot,'DefaultAxesTickLabelInterpreter','LaTeX'); 
+set(groot,'DefaultLegendInterpreter','LaTeX');
+set(groot,'DefaultAxesFontName', 'LaTeX');
+
+set(groot,'DefaultLineLineWidth',1.5);
+set(groot,'DefaultAxesLineWidth',2);
+FG.FS = 40;
+set(groot,'DefaultAxesFontSize', FG.FS); 
+set(groot,'DefaultTextFontSize', FG.FS);
+set(groot,'DefaultLegendFontSize',FG.FS);
+
+
+set(groot,'DefaultFigurePaperUnits', 'points');
+set(groot,'DefaultFigureUnits', 'points');
+set(groot,'DefaultFigurePaperPosition', [0, 0, 800, 600]);
+set(groot,'DefaultFigurePosition', [0, 0, 800, 600]);
+
+% set(gcf, 'PaperPositionMode', 'auto');
+
+
+FG.ColorBB = [0.0 0.0 0.0]; FG.ColorB1 = [0.1 0.1 0.1];
+FG.ColorB2 = [0.2 0.2 0.2]; FG.ColorB3 = [0.3 0.3 0.3];
+FG.ColorB4 = [0.4 0.4 0.4]; FG.ColorB5 = [0.5 0.5 0.5];
+FG.ColorB6 = [0.6 0.6 0.6]; FG.ColorB7 = [0.7 0.7 0.7];
+FG.ColorB8 = [0.8 0.8 0.8]; FG.ColorB9 = [0.9 0.9 0.9];
+FG.ColorBW = [1.0 1.0 1.0];
+
+FG.TickLength   = [0.025 0.025];
+
+
+FG.Color1 = [0 0.447058826684952 0.74117648601532];
+FG.Color2 = [0.850980401039124 0.325490206480026 0.0980392172932625];
+FG.Color3 = [0.929411768913269 0.694117665290833 0.125490203499794];
+FG.Color4 = [0.494117647409439 0.184313729405403 0.556862771511078];
+FG.Color5 = [0.466666668653488 0.674509823322296 0.18823529779911];
+FG.Color6 = [0.301960796117783 0.745098054409027 0.933333337306976];
+FG.Color7 = [0.635294139385223 0.0784313753247261 0.184313729405403];
